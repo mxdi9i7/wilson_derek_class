ce29d34049c28e3074a40639cd7cb796fea2b495
mxdi9i7 <peter.zheng88228@gmail.com>
2018-10-28 16:12:34 -0400

    initial

---
+++ class1/index.js
@@ -0,0 +1,78 @@
+// console.log('hide on bush');
+
+// String
+// const name = 'Wilson Zhang'
+// const name2 = 'Derek Hu'
+// const greeting = 'is fat.'
+// const grade = 'A'
+// console.log(name + greeting)
+// console.log('Derek has a ' + grade + ' in technology')
+// Numbers
+// const age = 11
+// console.log(name + ' and ' + name2 + ' are ' + age + ' years old.')
+// console.log(`${name} and ${name2} are ${age + 12} years old`)
+// Booleans
+
+
+// const isMarried = false
+// console.log(`${name} is ${isMarried ? 'married' : 'not married'}`)
+
+
+// Arrays
+// const girls = ['Fiona', 'Melissa', 'Kaitlyn', 'Brianna', 'Leah']
+
+for( let x = 0; x < girls.length; x ++) {
+  console.log(girls[x])
+}
+
+// for (let x = 4; x >= 0; x --) {
+//   console.log(girls[x])
+// }
+
+// girls.map((item) => {
+//   console.log(item + ' is chopped up in half')
+// })
+// const grades = [100, 100, 75, 100, 94, 0, 100, 65, 100]
+// grades.map(item => {
+//   if (item == 100) {
+//     console.log('Derek got a perfect score which is ' + item)
+//   } else {
+//     console.log('Derek is a loser he got ' +  item)
+//   }
+// })
+
+// Objects
+// const person1 = {
+//   name: 'wilson zhang',
+//   eyeColor: 'black',
+//   hairColor: 'black',
+//   hasGirlfriend: false,
+//   age: 11,
+//   isFat: true,
+//   favoriteCars: [
+//     {
+//       model: 'Model X',
+//       manufacturer: 'Tesla'
+//     },
+//     {
+//       model: 'GLE550',
+//       manufacturer: 'Mercedes'
+//     },
+//     {
+//       model: 'Veyron',
+//       manufacturer: 'Bughatti'
+//     }
+//   ]
+// }
+
+// const person2 = {
+//   name: 'Derek Hu',
+//   nickname: 'who',
+//   eyeColor: 'black',
+//   hairColor: 'cornflowerblue',
+//   hasGirlfriend: false,
+//   isGay: false,
+//   isFat: false,
+//   favoriteGirls: girls
+// }
+
