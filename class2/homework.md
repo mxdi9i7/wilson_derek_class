e9da8540e098eabd83ebb54afa1c156220e09ba4
mxdi9i7 <peter.zheng88228@gmail.com>
2018-11-04 16:05:50 -0500

    update on class 2

---
+++ class2/homework.md
@@ -0,0 +1,27 @@
+# Homework
+Read the material before you watch the video
+
+## Watch tutorial videos
+You have to follow the tutorial and create a React App.
+
+1. https://www.youtube.com/watch?v=rUdtgnwrA14
+2. https://www.youtube.com/watch?v=KdSVvWUvNos
+3. https://www.youtube.com/watch?v=JGgiC-_KYr0
+4. https://www.youtube.com/watch?v=rmOpBg5W1RU
+
+## Recap what we learn from last 2 weeks
+
+1. Data structures
+   - Strings
+   - Numbers
+   - Booleans
+   - Array
+   - Object
+2. For Loops
+3. Map functions
+4. Functions (addion, average, sum)
+
+## Reading
+
+1. https://ihatetomatoes.net/react-tutorial-for-beginners/
+
---
+++ class2/index.js
@@ -0,0 +1,41 @@
+// const number1 = 20
+// const number2 = 30
+
+// const addition = (a, b) => {
+//   return a + b - 10;
+// }
+
+// console.log(addition(number1, number2))
+
+const scores = [90, 100, 56, 76, 5, 20, 3]
+
+// const findSum = (arr) => {
+//   let result = 1;
+//   for (let i = 0; i < arr.length; i ++) {
+//     result = result + arr[i]
+//   }
+//   return result;
+// }
+
+// console.log(findSum(scores))
+
+// const friends = ['James', 'Peter', 'Derek', 'Wilson', 'Daniel']
+
+// const callingNames = (arr) => {
+
+// }
+
+// Make a function that calls each of these names like: I know Jack, we are best friends.
+
+// Make a function called Trumpify and it will turn your 
+// friends last names into trump. then console log the names
+
+// const findAverage = (arr) => {
+//   return arr.reduce((acc, sum) => (acc + sum)) / arr.length
+// }
+
+// console.log(findAverage(scores))
+
+x % 2 === 0 ? 'even' : 'odd'
+
+
